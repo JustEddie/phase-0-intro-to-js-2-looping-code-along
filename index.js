ea45acf2889d23e6f3234263a8a54a6bc943f025
JustEddie <skdpwls830@gmail.com>
2022-07-16 13:34:40 +1000

    commit tree 9727eeb771706ff0adbdf6ea6a36579e62110802

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function countDown(int) {
+    for(let i=int; i>=0; i--){
+        console.log(i);
+    }
+}
+
+function writeCards(arr,message) {
+    let arra = [];
+    for(let i=0; i<arr.length; i++){
+        arra.push("Thank you, " + arr[i] + ", for the wonderful "+message+" gift!")
+    }
+    return arra;
+}
+
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
